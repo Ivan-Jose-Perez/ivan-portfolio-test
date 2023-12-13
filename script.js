8f7ed29d01aa8c23a23e752e67bc76b37f201f26
Ivan-Jose-Perez <ivan.jose@ivanjp.tech>
2023-12-12 20:09:22 -0800

    Adding rotating photos to home page and sizing headers

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Welcome to IJP's personal website!");
+  console.log("Welcome to IJP's personal website!");
 
-});
-let currentPhoto = 0;
-const photos = document.querySelectorAll('.photo-rotator img');
-setInterval(() => {
-  photos[currentPhoto].style.display = 'none';
-  currentPhoto = (currentPhoto + 1) % photos.length;
+  let currentPhoto = 0;
+  const photos = document.querySelectorAll('.photo-rotator img');
+
+  // Initially show the first image
   photos[currentPhoto].style.display = 'block';
-}, 3000);
+
+  setInterval(() => {
+      photos[currentPhoto].style.display = 'none';
+      currentPhoto = (currentPhoto + 1) % photos.length;
+      photos[currentPhoto].style.display = 'block';
+  }, 3000); // Rotate image every 3 seconds
+});
+ 
